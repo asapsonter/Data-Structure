7bc6be823319437a54f3861cf447c3be0f911c2d
Sam <aarga@sycratech.com>
2023-12-11 09:03:16 +0800

    change 11-12-2023

---
+++ test2.py
@@ -0,0 +1,21 @@
+# l  = []
+# l.insert(0,1)
+# l.append(7)
+# l.append(9)
+# l.insert(2,3)
+# l.insert(0,8)
+
+# l[0] = 'haha'
+# print(l)
+
+# if len(l) >= 3:
+#     for i, j in enumerate(l):
+#         if i == 0:
+
+#             print(j)
+
+s = ' sam-here-we'
+
+t = s.split('-')
+
+print(t)
